e92bc78053f8cebf5da479ca400bcaac2e57ee74
Bhavya-g007 <bhavyags007@gmail.com>
2025-10-19 19:24:50 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+resource "aws_instance" "terraform-ec2" {
+    ami = "ami-0645cf88151eb2007"
+    key_name = "Inst1"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "Ec2-terraform"
+    }
+}
+output "ec2_public_ip" {
+  value = aws_instance.terraform-ec2.public_ip
+}
