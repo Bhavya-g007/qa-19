61058d35210e5f4961e5814c2155ae937627fc61
Bhavya-g007 <bhavyags007@gmail.com>
2025-10-20 05:58:28 +0530

    Add security group to EC2 instance configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,10 +5,13 @@ resource "aws_instance" "terraform-ec2" {
     ami = "ami-0645cf88151eb2007"
     key_name = "Inst1"
     instance_type = "t2.micro"
-    tags = {
+    security_groups = [aws_security_group.check.id]
+
+tags = {
         Name = "Ec2-terraform"
     }
 }
+
 output "ec2_public_ip" {
   value = aws_instance.terraform-ec2.public_ip
 }
