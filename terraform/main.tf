34f560f6266ddf0847f6c256708e206527fc9020
Bhavya-g007 <bhavyags007@gmail.com>
2025-10-20 06:31:10 +0530

    Update security group reference in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,8 @@ resource "aws_instance" "terraform-ec2" {
     ami = "ami-0645cf88151eb2007"
     key_name = "Inst1"
     instance_type = "t2.micro"
-    security_groups = [aws_security_group.check.id]
+
+    security_groups = ["check"]
 
 tags = {
         Name = "Ec2-terraform"
